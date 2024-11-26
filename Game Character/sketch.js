b815fa2c7d4903503a3c87bb18e0cf4639873411
JoshuaZSong <joshuasong94@outlook.com>
2024-11-26 13:49:01 -0700

    Game_Character_and_actions

---
+++ Game Character/sketch.js
@@ -0,0 +1,158 @@
+/*
+Author： Joshua Song
+The Game Project
+
+Game character
+
+Use p5 drawing functions such as rect, ellipse, line, triangle and
+point to draw the different states of your game character.
+
+Write the code so that your character appears inside the box for each
+state.
+
+*/
+
+var gameChar_x = 0;
+var gameChar_y = 0;
+
+function setup()
+{
+	createCanvas(400, 600);
+}
+
+function draw()
+{
+	background(255);
+
+	//Standing, facing frontwards
+
+	// stroke(100);
+	// noFill();
+	// rect(20, 60, 50, 80);
+	// noStroke();
+	fill(0);
+	text("1. standing front facing", 20, 160);
+
+	gameChar_x = 45;
+	gameChar_y = 137;
+	//Add your code here ...
+	// body
+	fill(139, 69, 19); // brown
+	stroke(0);
+	strokeWeight(3);
+	ellipse(gameChar_x + 40, gameChar_y - 50, 80, 80); //body
+
+	//arms and legs
+	strokeWeight(4);
+	line(gameChar_x + 30, gameChar_y - 30, gameChar_x + 10, gameChar_y - 10);//left arm
+	line(gameChar_x + 10, gameChar_y - 10, gameChar_x + 15, gameChar_y + 10);//left arm
+	line(gameChar_x + 60, gameChar_y - 30, gameChar_x + 70, gameChar_y - 10);//right arm
+	line(gameChar_x + 70, gameChar_y - 10, gameChar_x + 65, gameChar_y + 10);//right arm 
+	line(gameChar_x + 4, gameChar_y - 30, gameChar_x - 15, gameChar_y - 15);//left leg
+	line(gameChar_x - 15, gameChar_y - 15, gameChar_x - 10, gameChar_y + 10);//right leg  
+	line(gameChar_x + 50, gameChar_y - 10, gameChar_x + 45, gameChar_y + 10);//right leg 
+
+	// face
+	strokeWeight(3);
+	ellipse(gameChar_x + 65, gameChar_y - 70, 55, 48); //head
+	fill(225,0,0); 
+	strokeWeight(2);
+	arc(gameChar_x + 41, gameChar_y - 63, 80, 50, radians(-2), radians(27))// mouth
+	arc(gameChar_x + 65, gameChar_y - 70, 55, 48, radians(75) ,radians(167))// mouth
+	noStroke();
+	fill(139, 69, 19);
+	triangle(gameChar_x + 65, gameChar_y - 71, 
+			gameChar_x + 39, gameChar_y - 64, 
+			gameChar_x + 68, gameChar_y - 64)
+	stroke(0);
+	strokeWeight(2);
+	line(gameChar_x + 39, gameChar_y - 64, 
+		gameChar_x + 81, gameChar_y - 65);	
+	//teeth
+	strokeWeight(1);
+	fill(225,0,0); 
+	beginShape();
+	vertex(gameChar_x + 46, gameChar_y - 63);
+	vertex(gameChar_x + 51, gameChar_y - 50);
+	vertex(gameChar_x + 56, gameChar_y - 64);
+	vertex(gameChar_x + 61, gameChar_y - 50);
+	vertex(gameChar_x + 66, gameChar_y - 64);
+	vertex(gameChar_x + 71, gameChar_y - 50);
+	vertex(gameChar_x + 76, gameChar_y - 64);
+	endShape();
+	//eyes
+	stroke(240, 125, 0);
+	strokeWeight(5);
+	point(gameChar_x + 75, gameChar_y - 75);
+	point(gameChar_x + 55, gameChar_y - 75);
+
+	//tail magic hand
+
+
+
+
+	//Jumping facing forwards
+	stroke(100);
+	noFill();
+	rect(220, 60, 50, 80);
+	noStroke();
+	fill(0);
+	text("2. jumping facing forwards", 220, 160);
+
+	gameChar_x = 245;
+	gameChar_y = 137;
+	//Add your code here ...
+
+
+	//Walking, turned left
+	stroke(100);
+	noFill();
+	rect(20, 260, 50, 80);
+	noStroke();
+	fill(0);
+	text("3. Walking left", 20, 360);
+
+	gameChar_x = 45;
+	gameChar_y = 337;
+	//Add your code here ...
+
+
+	//Walking, turned right
+	stroke(100);
+	noFill();
+	rect(220, 260, 50, 80);
+	noStroke();
+	fill(0);
+	text("4. Walking right", 220, 360);
+
+	gameChar_x = 245;
+	gameChar_y = 337;
+	//Add your code here ...
+
+
+	//Jumping right
+	stroke(100);
+	noFill();
+	rect(20, 460, 50, 80);
+	noStroke();
+	fill(0);
+	text("5. Jumping to the right", 20, 560);
+
+	gameChar_x = 45;
+	gameChar_y = 537;
+	//Add your code here ...
+
+
+	//Jumping to the left
+	stroke(100);
+	noFill();
+	rect(220, 460, 50, 80);
+	noStroke();
+	fill(0);
+	text("6. Jumping to the left", 220, 560);
+
+	gameChar_x = 245;
+	gameChar_y = 537;
+	//Add your code here ...
+
+}
